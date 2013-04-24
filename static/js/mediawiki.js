530287ff78667b16310352891f954f0d0bb66890
John McLear <john@mclear.co.uk>
2013-04-24 14:13:36 +0100

    support for url param

--- static/js/mediawiki.js
+++ static/js/mediawiki.js
@@ -26,6 +26,29 @@ exports.postAceInit = function(hook, context){
       mediawiki.disable();
     }
   });
+  /* Param initiator */
+  var urlContainsMediawikiTrue = (getParam("mediawiki") == "true"); // if the url param is set
+   if(urlContainsMediawikiTrue){
+    $('#options-mediawiki').attr('checked','checked');
+    mediawiki.enable();
+  }else if (getParam("mediawiki") == "false"){
+    $('#options-mediawiki').attr('checked',false);
+    mediawiki.disable();
+  }
 }
 
 exports.aceEditorCSS = function(hook_name, cb){return ["/ep_mediawiki/static/css/mediawiki.css"];} // inner pad CSS
+
+function getParam(sname){
+  var params = location.search.substr(location.search.indexOf("?")+1);
+  var sval = "";
+  params = params.split("&");
+  // split param and value into individual pieces
+  for (var i=0; i<params.length; i++)
+  {
+    temp = params[i].split("=");
+    if ( [temp[0]] == sname ) { sval = temp[1]; }
+  }
+  return sval;
+}
+
